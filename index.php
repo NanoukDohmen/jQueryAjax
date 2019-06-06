dc146f3c2d3c912ab700db743be5e349d72b0c02
Nanouk <51449596+NanoukDohmen@users.noreply.github.com>
2019-06-06 17:09:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+
+<html>
+    <?php include 'template-parts/head.php';?>
+
+    <body>
+
+        <?php include 'template-parts/main_part.php';?>
+
+	</body>
+
+    <?php include 'template-parts/footer.php';?>
+
+</html>
