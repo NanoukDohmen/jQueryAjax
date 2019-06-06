33175de7eba9840c3c465d4b401af593db4863e6
Nanouk <51449596+NanoukDohmen@users.noreply.github.com>
2019-06-06 17:09:19 +0200

    Add files via upload

---
+++ page/about.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+
+<html>
+    <?php include '../template-parts/head.php';?>
+
+    <body>
+
+        <div class="article">
+            <h2>200 Words</h2>
+                <span class="enter"></span>
+            <p>
+                Dedicate yourself to lifelong fucking learning. Never, never assume that what you have achieved
+                is fucking good enough. Paul Rand once said, “The public is more familiar with bad fucking design than good
+                design. It is, in effect, conditioned to prefer bad design, because that is what it lives with.
+                The new becomes threatening, the old reassuring.” Don’t get hung up on things that don’t fucking work.
+                The details are not the details. They make the fucking design. Make your work consistent but not fucking predictable.
+                While having drinks with Tibor Kalman one night, he told me, “When you make something no one hates, no one fucking loves it.”</p>
+
+                <span class="enter"></span>
+
+            <p> You are not your fucking work. You won’t get good at anything by doing it a lot fucking       aimlessly.
+                Widows and orphans are terrible fucking tragedies, both in real life and definitely in typography.
+                Don’t worry about what other people fucking think. To go partway is easy, but mastering anything requires hard
+                fucking work. If you’re not being fucking honest with yourself how could you ever hope to communicate
+                something meaningful to someone else?
+                Can we all just agree as the greater design community to stop fucking talking about Comic Sans</p>
+        </div>
+
+
+    </body>
+
+</html>
